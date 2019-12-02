66354e609f461b4ea043fd8b7892be359c709ac1
Orhan Jan <orhancan85@gmail.com>
2019-12-02 21:57:31 +0400

    Auto Complete Field added in order to choose Source and Destionation points

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/constraintlayout/widget/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f0a0049;
-        public static final int end = 0x7f0a0069;
-        public static final int gone = 0x7f0a0078;
-        public static final int invisible = 0x7f0a0085;
-        public static final int left = 0x7f0a008a;
-        public static final int packed = 0x7f0a00a4;
-        public static final int parent = 0x7f0a00a6;
-        public static final int percent = 0x7f0a00a9;
-        public static final int right = 0x7f0a00ae;
-        public static final int spread = 0x7f0a00d2;
-        public static final int spread_inside = 0x7f0a00d3;
-        public static final int start = 0x7f0a00d8;
-        public static final int top = 0x7f0a00f4;
-        public static final int wrap = 0x7f0a0105;
+        public static final int bottom = 0x7f0a004b;
+        public static final int end = 0x7f0a006b;
+        public static final int gone = 0x7f0a007a;
+        public static final int invisible = 0x7f0a0087;
+        public static final int left = 0x7f0a008c;
+        public static final int packed = 0x7f0a00a6;
+        public static final int parent = 0x7f0a00a8;
+        public static final int percent = 0x7f0a00ab;
+        public static final int right = 0x7f0a00b0;
+        public static final int spread = 0x7f0a00d4;
+        public static final int spread_inside = 0x7f0a00d5;
+        public static final int start = 0x7f0a00da;
+        public static final int top = 0x7f0a00f6;
+        public static final int wrap = 0x7f0a0107;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/com/example/mapversitydemo/NavScreen.java
+++ app/src/main/java/com/example/mapversitydemo/NavScreen.java
@@ -1,9 +1,12 @@
 package com.example.mapversitydemo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -15,18 +18,14 @@ import com.github.chrisbanes.photoview.PhotoViewAttacher;
 
 public class NavScreen extends Fragment {
 
-    ImageView imageview;
-
+    private static final String[] ROOMS = new String[]{"A210", "A209", "A202", "A201",
+            "Locker Room", "WC", "Seminar Rooms", "Elevators"
+    };
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navigation,container,false);
-
-
-
-
-
         return view;
     }
 
@@ -35,5 +34,15 @@ public class NavScreen extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         PhotoView photoView = view.findViewById(R.id.mapA201);
         photoView.setImageResource(R.drawable.a201lala);
+
+        AutoCompleteTextView srcPointView = view.findViewById(R.id.SourcePoint);
+        ArrayAdapter<String> srcAdapter = new ArrayAdapter<String>(this.getContext(),android.R.layout.simple_list_item_1,ROOMS);
+        srcPointView.setAdapter(srcAdapter);
+
+        AutoCompleteTextView destPointView = view.findViewById(R.id.DestinationPoint);
+        ArrayAdapter<String> destAdapter = new ArrayAdapter<String>(this.getContext(),android.R.layout.simple_list_item_1,ROOMS);
+        destPointView.setAdapter(destAdapter);
+
+
     }
 }
